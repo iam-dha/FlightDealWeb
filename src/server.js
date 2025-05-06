3f726ce288cc317bc7b172af981f49bd60d701d1
Bui Hoang Anh <Anh.BH215298@sis.hust.edu.vn>
2025-05-07 00:06:16 +0700

    tạo scheme cho các models như user, promotion, flight và setup core application

---
+++ src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();  
+const app = require('./app');  
+const mongoose = require('mongoose');  
+  
+// Connect to MongoDB  
+mongoose.connect(process.env.MONGODB_URI)  
+  .then(() => console.log('MongoDB connected'))  
+  .catch(err => console.error('MongoDB connection error:', err));  
+  
+const PORT = process.env.PORT || 5000;  
+app.listen(PORT, () => {  
+  console.log(`Server running on port ${PORT}`);  
+});
